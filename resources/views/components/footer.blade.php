770129934fb08ff194b6e1967b45e56d86b44a91
Fauzy169 <ahmadfauzy169@gmail.com>
2025-11-10 19:09:07 +0800

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,60 @@
+<div class="footer footer-1">
+        <div class="container">
+            <div class="footer-inner">
+                <div class="row">
+                    <div class="col-xl-3 col-md-6 col-sm-6 box-widget-col">
+                        <div class="footer-widget footer-box-widget">
+                            <div class="footer-logo"><img src="{{ asset('assets/images/logo1.svg') }}" alt="footer-logo"></div>
+                            <p>Solid is the information & experience directed at an end-user</p>
+                            <ul class="social-links-footer2">
+                                <li><a class="platform fb" target="_blank" href="http://facebook.com"><i
+                                            class="fab fa-facebook"></i></a>
+                                </li>
+                                <li><a class="platform yt" target="_blank" href="http://youtube.com"><i
+                                            class="fab fa-youtube"></i></a>
+                                </li>
+                                <li><a class="platform ttr" target="_blank" href="http://twitter.com"><i
+                                            class="fab fa-twitter"></i></a>
+                                </li>
+                                <li><a class="platform lkd" target="_blank" href="http://linkedin.com"><i
+                                            class="fab fa-linkedin"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 col-sm-6 box-widget-col2">
+                        <div class="footer-widget">
+                            <h3 class="footer-widget-title">About Us</h3>
+                            <p class="widget-text">Elegant pink origami design three type
+                                of dimensional view and decoration co
+                                Great for adding a decorative touch to
+                                any room’s decor.</p>
+                            <a href="#0" class="getin-touch">Get In Touch <i class="fal fa-long-arrow-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-13 col-md-6 col-sm-6 no-gutters">
+                        <div class="footer-widget">
+                            <h3 class="footer-widget-title">Information</h3>
+                            <ul class="widget-items cata-widget">
+                                <li class="widget-list-item"><a href="#0">About</a></li>
+                                <li class="widget-list-item"><a href="#0">FAQ's</a></li>
+                                <li class="widget-list-item"><a href="#0">Wishlist</a></li>
+                                <li class="widget-list-item"><a href="#0">Cart</a></li>
+                                <li class="widget-list-item"><a href="#0">Checkout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-13 col-md-6 col-sm-6 no-gutters">
+                        <h3 class="footer-widget-title">My Account</h3>
+                        <ul class="footer-widget">
+                            <li class="widget-list-item"><a href="#0">Wishlist</a></li>
+                            <li class="widget-list-item"><a href="#0">Cart</a></li>
+                            <li class="widget-list-item"><a href="#0">Checkout</a></li>
+                            <li class="widget-list-item"><a href="#0">My Account</a></li>
+                            <li class="widget-list-item"><a href="#0">Shop</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" class="darkmode" data-theme="light">
+
+<x-head headTitle='{{ isset($headTitle) ? $headTitle : "" }}' css='{!! isset($css) ? $css : "" !!}'/>
+
+<body>
+
+    <!-- ..::Preloader Section Start Here::.. -->
+    <x-preloader />
+    <!-- ..::Preloader End Here::.. -->
+
+    <div class="anywere"></div>
+
+    @include('layout.header')
+
+    @yield('content')
+
+        <?php 
+
+            if (!isset($footer)) {
+                ?>
+                <x-footer/>
+                <?php
+            }
+        ?>
+
+    <x-scroll />
+
+    <x-script script='{!! isset($script) ? $script : "" !!}' />
+
+</body>
+
+</html>
